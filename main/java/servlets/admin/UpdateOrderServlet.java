f7977a5241ac5ae7aa70a3c5c82cb604051e5e1a
AmirMusairov <riko.hs96@gmail.com>
2020-01-04 05:37:27 +0100

    Rework

---
+++ main/java/servlets/admin/UpdateOrderServlet.java
@@ -0,0 +1,24 @@
+package servlets.admin;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import agency.dao.OrderDao;
+import agency.dao.impl.DefaultOrderDao;
+
+public class UpdateOrderServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		int userId = Integer.parseInt(request.getParameter("userId"));
+		int orderId = Integer.parseInt(request.getParameter("orderId"));
+
+		OrderDao orderDao = DefaultOrderDao.getInstance();
+		orderDao.updateOrderData(orderId, userId);
+	}
+}
